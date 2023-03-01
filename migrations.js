93a9e27c52c7b630a6777b9bcd10e5b94198c92e
aruss <arusslabs@gmail.com>
2023-03-01 11:19:18 +0100

    ...

---
+++ migrations.js
@@ -0,0 +1,28 @@
+function migrate(schema, toVersion, migrations) {
+  const fromVersion = schema.version;
+  const direction = upOrDown(fromVersion, toVersion);
+  if (direction === "same") {
+    return schema;
+  }
+  const currentMigration = migrations.find(
+    (migration) => migration[direction === "up" ? "from" : "to"] === fromVersion
+  );
+  const newSchema = currentMigration[direction](schema);
+  return migrate(newSchema, toVersion);
+};
+
+function upOrDown  (fromVersion, toVersion)  {
+  const fromNumbers = fromVersion.split(".").map((el) => Number(el));
+  const toNumbers = toVersion.split(".").map((el) => Number(el));
+  for (let i = 0; i < fromNumbers.length; i++) {
+    if (fromNumbers[i] < toNumbers[i]) {
+      return "up";
+    }
+    if (fromNumbers[i] > toNumbers[i]) {
+      return "down";
+    }
+  }
+  return "same";
+};
+
+module.exports = migrate; 
---
+++ migrations.test.js
@@ -0,0 +1,48 @@
+const migrate = require("./migrations");
+
+const migrationsForA = [
+  {
+    from: "1",
+    to: "2",
+    up: (schema) => {
+      const newSchema = {
+        version: "2",
+        person: {
+          name: schema.person.name,
+        },
+        pets: {
+          ...schema.person.pets,
+        },
+      };
+      return newSchema;
+    },
+    down: (schema) => {
+      const newSchema = {
+        version: "1",
+        person: {
+          ...schema.person,
+          pets: { ...schema.pets },
+        },
+      };
+      return newSchema;
+    },
+  },
+];
+
+const stateAOld = {
+  version: "1",
+  person: {
+    name: "Edgar",
+    pets: {
+      type: "dog",
+      name: "Daffodil",
+    },
+  },
+};
+
+test("migrate thing a", () => {
+  console.log(migrate);
+  const migrated = migrate(stateAOld, "2", migrationsForA);
+
+  console.log(migrated);
+});
